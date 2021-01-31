072c18338925266273379cc0f9b9cb5087a78d83
nikolaVlad <nikolavlad63@gmail.com>
2021-01-31 02:13:57 +0100

    Index stranica napravljena i dodate osnovne funkcionalnosti

--- Templejti/static/script.js
+++ Templejti/static/script.js
@@ -1,15 +1,74 @@
-// Funkcija za prikaži/sakrij shearch bar u Navigaciji
 
-var showShearchBar = () =>
+
+
+
+// Funkcija za prikaz Shop Menia
+var showShopMenu = () =>
+{
+  var x = document.getElementById("shopMenu");
+  x.style.display = "block";
+}
+
+// Funkcija za sakrivanje Shop menia
+var hideShopMenu = () =>
+{
+  var x = document.getElementById("shopMenu");
+  x.style.display = "none";
+}
+
+// Funkcija za prikazivanje/sakrivanje Shop menia
+var showHideShop = () =>
 {
   
-    var x = document.getElementById("shearchBar");
+    var x = document.getElementById("shopMenu");
     if (x.style.display === "none") {
-      x.style.display = "block";
+     showShopMenu();
     } 
     else {
-        x.style.display = "none";
+       hideShopMenu();
     }
-
-   
 }
+
+
+// Funkcija : Prikaži/Sakrij lozinku
+var showHidePass = () =>
+{
+  var x = document.getElementById("pass");
+  
+
+  if(x.type == "text")
+  {
+    x.type = "password";
+  }
+
+  else
+  {
+    x.type = "text";
+    
+  }
+
+}
+
+
+
+// Funkcija koja na index stranici u slajderu prikazuje podatke o artiklu kad se misem predje preko slajdera
+var showArticleInfo = (...args) =>
+{
+  for(x of args)
+  {
+    document.getElementById(x).style.display = "block"
+  }
+
+}
+
+// Funkcija koja na index stranici u slajderu SAKRIVA podatke o artiklu kad se misem predje preko slajdera
+var hideArticleInfo = (...args) =>
+{
+  for(x of args)
+  {
+    document.getElementById(x).style.display = "none"
+  }
+}
+
+
+
