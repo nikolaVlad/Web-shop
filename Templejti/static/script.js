6c232bcd045cdebbce6817b534bd1ec7dcca881a
nikolaVlad <nikolavlad63@gmail.com>
2021-02-01 13:32:11 +0100

    Dodat fixirani footer i ScrollUp dugme

--- Templejti/static/script.js
+++ Templejti/static/script.js
@@ -73,3 +73,26 @@ var hideArticleInfo = (...args) =>
 
 
 
+// Funkcija za scroll Up na svakoj stranici
+$(document).ready(function() 
+{
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) 
+    {
+      $('#toTopBtn').fadeIn();
+    }
+    else {
+      $('#toTopBtn').fadeOut();
+    }
+  });
+
+
+  
+  $('#toTopBtn').click(function() {
+    $("html, body").animate({
+    scrollTop: 0
+    }, 300);
+  
+    return false;
+  });
+});
